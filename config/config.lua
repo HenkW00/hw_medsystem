295f16eaf175bd641e04d0907ac4ce5f203e6057
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-04-19 18:57:20 +0200

    Fix

--- config/config.lua
+++ config/config.lua
@@ -2,15 +2,15 @@ Config = {}
 
 Config.Debug = true
 Config.checkForUpdates = true
-Config.Webhook = "https://discord.com/api/webhooks/1230824436614037524/vQ0o3hUeR_CqKgoGbEpBNBKqFwIEuAvjzdq3fOkuba-xpmW03wKzC8CkXFiGla5b3Ijx"
+Config.Webhook = "https://discord.com/api/webhooks/1230924865368756285/rVjEew3Gk_kBIMLPjHgmLPtPCXwwsDGb6q6QNcMhovmPMONvCLgT7GpVx8aILATR6hO9"
 
 Config.Declared = 'Declared Dead. Please Respawn!' -- This Msg appeared when the player died using /med [id] to display blood = 0 - 5% and Hurt area is Head
 Config.Timer = 8   -- Timer to Remove Med Display after using Med System
 
 Config.jobs = {
-		names = {
+	names = {
 		ambulance = true, 
 		police =  true, 
 		kmar = true,
-		}
+	}
 }
